183ff056491d5692189ee558e80d1793fef3e6d7
CristianFernando <gordyfernando@gmail.com>
2022-06-08 17:07:48 -0300

    mi segundo commit

--- src/TestApp.java
+++ src/TestApp.java
@@ -4,6 +4,7 @@ public class TestApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bienvenidos a Git");
+		System.out.println("Status");
 	}
 
 }
