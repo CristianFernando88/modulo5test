8eb330c4dae4814d841b6bb3c8351e6d1d6e2f67
CristianFernando <gordyfernando@gmail.com>
2022-06-17 13:50:46 -0300

    clase de cristian

---
+++ src/TestApp2.java
@@ -0,0 +1,12 @@
+
+public class TestApp2 {
+	private String dato1;
+	private String dato2;
+	
+	public String getDato1() {
+		return this.dato1;
+	}
+	public void setDato1(String dato) {
+		this.dato1=dato;
+	}
+}
