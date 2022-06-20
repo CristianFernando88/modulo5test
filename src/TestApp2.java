6206b57aa599b9572bd314def4dff3f4ee9d0df2
CristianFernando88 <107149744+CristianFernando88@users.noreply.github.com>
2022-06-20 12:54:09 -0300

    Update TestApp2.java

--- src/TestApp2.java
+++ src/TestApp2.java
@@ -9,4 +9,10 @@ public class TestApp2 {
 	public void setDato1(String dato) {
 		this.dato1=dato;
 	}
+	public String getDato2() {
+		return this.dato2;
+	}
+	public void setDato2(String dato) {
+		this.dato2=dato;
+	}
 }
